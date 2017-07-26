3dfb4652e19da4408cca49cacdc0f2be5ee07afa
Kiryl Ply <craft37@gmail.com>
2017-07-26 18:30:26 +0300

    add delegation to transaction model, delete ugly links hover

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,5 @@
 class Account < ApplicationRecord
+  belongs_to :user
   has_many :transactions
   has_many :roles
   has_many :users, through: :roles
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
-  has_many :transactions
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable   # , :confirmable
+         :recoverable, :rememberable, :trackable, :validatable # , :confirmable
 
+  has_many :transactions, through: :accounts
   has_many :roles
   has_many :accounts, through: :roles
 end

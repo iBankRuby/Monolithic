58810707ea3045da17d2437b4c1e41f46a311aec
ConstVol <konstantinvolodkevich@gmail.com>
2017-07-31 18:49:15 +0300

    transferred co-user's invitation to account#index

--- spec/controllers/invites_controller_spec.rb
+++ spec/controllers/invites_controller_spec.rb
@@ -1,3 +1,22 @@
-RSpec.describe InvitesController, type: :controller do
+require 'rails_helper'
+require 'pry'
 
+RSpec.describe InvitesController, type: :controller do
+  describe 'PATCH update' do
+    let! :user do
+      create :user
+    end
+    before :each do
+      sign_in user
+      FactoryGirl.create :user, :id => 1, :email => 'me55@mail.ru'
+      @user = FactoryGirl.create :user, :id => 2,  :email => 'me3@mail.ru'
+      @invite = FactoryGirl.create :invite
+    end
+
+    it 'redirect to account' do
+      patch :update, params: { id: @invite.id }
+      expect(response).to redirect_to :accounts
+    end
+
+  end
 end

05052ae3f55a5ed1402192451c5a60e1bdb67c00
tanya-an <tanja.andrejewa@yandex.by>
2017-08-01 12:47:01 +0300

    added profile
    added progile.html.haml, profile_controller, modified routed.rb

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,8 @@
+class ProfileController < ApplicationController
+	attr_reader :accounts, :user
+
+	def index
+	  @user = current_user
+	  @accounts = user.accounts
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :transactions
   end
   devise_for :users
+
+  resources :profile
   
   root to: 'accounts#index'
 end

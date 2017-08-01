457109c053bd9a5c3cc2efbba7584834ff3f4b63
Kiryl Ply <craft37@gmail.com>
2017-08-01 09:17:42 +0300

    add railroady for controllers models diagrams

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ group :development do
   gem 'simplecov', require: false
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'railroady'
 end
 
 group :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 User.new(email: 'me@example.com', password: 'secret').save
 Invite.create!(user_from_id: 2,
                user_to_id: 1,
-              account_id: 1)
+               account_id: 1)

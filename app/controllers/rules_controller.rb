f92c64c9b1b9968c13936178c496749a54bbfdb7
Jack Molosh <jackmolosh@gmail.com>
2017-08-02 14:27:54 +0300

    Add rules create function

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -1,7 +1,7 @@
 class RulesController < ApplicationController
   before_action :set_rule, only: %i[edit update]
 
-  attr_reader :rule
+  attr_reader :rule, :rules
 
   def index
     @rules = Rule.all
@@ -12,10 +12,12 @@ class RulesController < ApplicationController
   end
 
   def create
-    @rule = Rule.new
+    @rule = Rule.new(rule_params)
 
     if rule.save
       redirect_to :account_rules
+    else
+      redirect_to :new_account_rule, notice: 'Invalid params.'
     end
   end
 
@@ -23,7 +25,9 @@ class RulesController < ApplicationController
 
   def update
     if rule.update(rule_params)
-      redirect_to :edit_account_rule, notice: 'Rules have updated'
+      redirect_to :edit_account_rule, notice: 'Rules have updated.'
+    else
+      redirect_to :edit_account_rule, notice: 'Rules have note updated. Invalid params.'
     end
   end
 
@@ -34,6 +38,6 @@ class RulesController < ApplicationController
   end
 
   def rule_params
-    params.fetch(:rules).permit()
+    params.fetch(:rules).permit(:spending_limit)
   end
 end

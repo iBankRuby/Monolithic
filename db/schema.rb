556e70695a92973ce2f573c73676be98c52ba272
Jack Molosh <jackmolosh@gmail.com>
2017-08-02 10:32:32 +0300

    Add limitation rule to rules db table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801154507) do
+ActiveRecord::Schema.define(version: 20170802072828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20170801154507) do
   create_table "rules", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "spending_limit", precision: 10, scale: 4
   end
 
   create_table "transactions", force: :cascade do |t|

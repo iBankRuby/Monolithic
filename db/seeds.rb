41d113a94e8335d63c0437435eefc91ad767e2bc
Jack Molosh <jackmolosh@gmail.com>
2017-08-03 13:55:29 +0300

    Add seeds for roles table

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,7 @@ User.new(email: 'me@example.com', password: 'secret').save
 Invite.create!(user_from_id: 2,
                user_to_id: 1,
                account_id: 1)
+
+%w[owner co-user].each do |el|
+  Role.create(name: el)
+end

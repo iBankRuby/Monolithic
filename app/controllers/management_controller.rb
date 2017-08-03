513a52b1c31e446e84eaecffd72034f500fd3ac8
mesmerze <aichernovmail@gmail.com>
2017-08-03 13:09:17 +0300

    Ref managment contr

--- app/controllers/management_controller.rb
+++ app/controllers/management_controller.rb
@@ -1,11 +1,17 @@
 class ManagementController < ApplicationController
+  before_action :set_account
 
   def index
-    @account = Account.find(params[:account_id])
-    @roles = Role.where(account_id: params[:account_id])
+    @roles = Role.where(account_id: @account.id)
   end
 
   def destroy
     Role.find_by(id: params[:id]).destroy
+  end
+
+  private
+
+  def set_account
+    @account = Account.find(params[:account_id])
   end
 end

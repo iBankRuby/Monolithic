9da2488c1384f36f7c4ed320c99eafec65649e8e
Alex <alexandrianovmail@gmail.com>
2017-08-03 17:09:51 +0300

    fixed spec fail

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
     resources :transactions, only: %i[create]
     resources :invites, only: %i[index index create destroy update]
     resources :management
-    resources :rules, except: %i[index destroy]
+    resources :rules, except: %i[destroy]
   end
 
   devise_for :users
 
   resources :profile
-  
+
   root to: 'accounts#index'
 end

4977b831a3ca4674847e561617c12b4a6b5b4ba2
Alex <alexandrianovmail@gmail.com>
2017-08-03 13:51:42 +0300

    devise local mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,15 +27,27 @@ Rails.application.configure do
   end
 
   # Do care if the mailer can't send.
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'root_url' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
-  config.action_mailer.delivery_method = :sendgrid_actionmailer
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.sendgrid_actionmailer_settings = {
     api_key: ENV['SENDGRID_API_KEY']
+  }
+
+  config.action_mailer.smtp_settings = {
+    user_name:      ENV['SENDMAIL_USERNAME'],
+    password:       ENV['SENDMAIL_PASSWORD'],
+    domain:         'localhost:3000',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 
   # Print deprecation notices to the Rails logger.

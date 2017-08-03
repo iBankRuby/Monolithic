d50b35f8596e35261b7ace743bc889b67787be70
Jack Molosh <jackmolosh@gmail.com>
2017-08-03 14:37:25 +0300

    Change travis config

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-User.new(email: 'me@example.com', password: 'secret').save
-Invite.create!(user_from_id: 2,
-               user_to_id: 1,
-               account_id: 1)
+# User.new(email: 'me@example.com', password: 'secret').save
+# Invite.create!(user_from_id: 2,
+#                user_to_id: 1,
+#                account_id: 1)
 
 %w[owner co-user].each do |el|
   Role.create(name: el)

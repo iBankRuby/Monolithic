5793d7328ef17890ca8a506a4882f3dd2ecb1299
Jack Molosh <jackmolosh@gmail.com>
2017-08-03 13:57:58 +0300

    Fix associations

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,12 +1,12 @@
 class AccountsController < ApplicationController
   before_action :set_account, only: %i[show destroy]
-  before_action :set_current_user, only: %i[index create]
+  before_action :set_current_user, only: %i[index create show]
 
   attr_reader :accounts, :user, :account, :income
 
   def index
     @accounts = user.accounts
-    @invites = Invite.where(user_to_id: current_user.id, status: nil)
+    @invites = Invite.where(user_to_id: user.id, status: nil)
   end
 
   def new
@@ -15,12 +15,12 @@ class AccountsController < ApplicationController
 
   def create
     account = Account.create!(iban: Forgery('credit_card').number, balance: 1000)
-    account.roles.create(user: current_user, role: 'owner')
+    account.account_users.create(user: user, role_id: Role.find_by_name('owner').id)
     redirect_to account, notice: 'Account was successfully created.'
   end
 
   def show
-    @transactions = Transaction.where(user_id: current_user.id, account_id: account.id)
+    @transactions = Transaction.where(user_id: user.id, account_id: account.id)
     @income = Transaction.where(remote_account_id: account.iban.to_s, status_to: false)
     @roles = Role.where(account_id: params[:id])
   end
@@ -37,6 +37,6 @@ class AccountsController < ApplicationController
   end
 
   def set_current_user
-    @user = User.find(current_user.id)
+    @user = current_user
   end
 end

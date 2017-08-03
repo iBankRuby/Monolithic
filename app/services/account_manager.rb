ddd944c2681551cc0cf39225246346ff0d9a06b8
ConstVol <konstantinvolodkevich@gmail.com>
2017-08-03 16:35:13 +0300

    created service to manage account_managemet view

---
+++ app/services/account_manager.rb
@@ -0,0 +1,29 @@
+class AccountManager
+  attr_reader :account_id, :user, :account_user
+  def initialize(user, account_id)
+    @user = user
+    @account_id = account_id
+  end
+
+  def manage
+    grant_access
+  end
+
+  private
+
+  def grant_access
+    role_type = check_role
+    get_users(role_type)
+  end
+
+  def check_role
+    @account_user ||= AccountUser.find_by(user_id: @user.id, account_id: @account_id)
+    role_type = @account_user.role.name
+  end
+
+  def get_users(role_type)
+    return [].push @account_user if role_type == 'co-user'
+    @account_users = AccountUser.where(account_id: @account_id)
+  end
+
+end

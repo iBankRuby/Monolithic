60b8b3ba5306183745650ab8e79f16f6692adb32
Jack Molosh <jackmolosh@gmail.com>
2017-08-07 17:43:51 +0300

    Simplify custom validation method

--- app/models/exceeding_request.rb
+++ app/models/exceeding_request.rb
@@ -4,16 +4,31 @@ class ExceedingRequest < ApplicationRecord
   validate :user_can_send_one_exceeding_request_per_period
   validates :amount, numericality: { greater_than_or_equal_to: 0 }
 
+  def user
+    account_user.user
+  end
+
+  def account
+    account_user.account
+  end
+
   private
 
   def user_can_send_one_exceeding_request_per_period
     current_day = Date.today
-    ExceedingRequest.where('created_at >= ? and created_at <= ?', current_day, current_day + 1).each do |req|
-      req_account_user = req.account_user
-      if req_account_user.user == account_user.user && req_account_user.account == account_user.account
+    ExceedingRequest.where('created_at >= ? and created_at <= ?', current_day, current_day + 1).each do |request|
+      if compare_user_with(request) && compare_account_with(request)
         errors.add(:created_at, 'You cannot create request twice in life period of limit')
         break
       end
     end
+  end
+
+  def compare_user_with(request)
+    request.user == user
+  end
+
+  def compare_account_with(request)
+    request.account == account
   end
 end

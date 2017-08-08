4c0dede10dbd81233a12e510efde6731fb4f4f4d
ConstVol <konstantinvolodkevich@gmail.com>
2017-08-08 20:32:23 +0300

    registrations_controller_spec fixes

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe RegistrationsController, type: :controller do
+  include Devise::Test::ControllerHelpers
+  before do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
 	describe 'POST create' do
     it 'has 302 status code' do
       post :create, params: { user: {name: "etetr", email: "4@www.ddd", password: "[FILTERED]", password_confirmation: "[FILTERED]"}, commit: "Sign up" }

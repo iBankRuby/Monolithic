ea4c2eeb4801439d089f4a98ce5fbd88d871c5e0
tanya-an <tanja.andrejewa@yandex.by>
2017-08-08 20:15:02 +0300

    added registrations_controllers_spec

---
+++ spec/controllers/registrations_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe RegistrationsController, type: :controller do
+	describe 'POST create' do
+    it 'has 302 status code' do
+      post :create, params: { user: {name: "etetr", email: "4@www.ddd", password: "[FILTERED]", password_confirmation: "[FILTERED]"}, commit: "Sign up" }
+      expect(response).to have_http_status :found
+    end
+
+    it 'render root' do
+      post :create, params: { user: {name: "etetr", email: "4@www.ddd", password: "[FILTERED]", password_confirmation: "[FILTERED]"}, commit: "Sign up" }
+      expect(response).to redirect_to :root
+    end
+  end
+end

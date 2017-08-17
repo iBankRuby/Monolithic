10775730465b7909a3dcbb5e899ae81af494ffab
mesmerze <aichernovmail@gmail.com>
2017-08-17 16:41:20 +0300

    Delayed job for trans statuses

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
-  domain: 'http://localhost:3000',
+  domain: 'http://54.69.43.92:3000',
   address: 'smtp.sendgrid.net',
   port: 587,
   authentication: :plain,
